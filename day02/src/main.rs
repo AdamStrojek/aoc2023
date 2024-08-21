922129d5fe49eeb1e72b44d13f0dcdd8b4ffcc2e
Adam Strojek <adam@strojek.info>
2024-08-21 15:43:25 +0200

    Refactoring, extract parsing game

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,25 +3,27 @@ use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let cubes = HashMap::from([("red", 12u8), ("green", 13u8), ("blue", 14u8)]);
+    let cubes = HashMap::from([("red".to_string(), 12u8), ("green".to_string(), 13u8), ("blue".to_string(), 14u8)]);
 
     solution1("example1.txt", &cubes);
     solution1("input.txt", &cubes);
 }
 
-fn solution1(filename: &str, max_cubes: &HashMap<&str, u8>) {
-    println!("Calibrating file {}", filename);
+struct Game {
+    game: u32,
+    cubes: HashMap<String, u8>,
+}
 
-    let mut result = 0;
-
-    for line in fs::read_to_string(filename).expect("Could not read file").lines() {
+impl Game {
+    pub fn from_line(line: &str) -> Self {
+        let mut cubes: HashMap<String, u8> = HashMap::new();
         // "Game 1: ....."
         //       ^ Move cursor to the first digit
         // All lines starts with "Game "
         let mut it = line[5..].chars();
 
         // Collect all digits to generate game number
-        let game: u8 = it.by_ref().take_while(|x| x.is_ascii_digit()).collect::<String>().parse().unwrap();
+        let game: u32 = it.by_ref().take_while(|x| x.is_ascii_digit()).collect::<String>().parse().unwrap();
 
         // Remaining characters in line buffer:
         // " 1 red, 1 green, 1 blue; 2 red, 1 green, 6 blue; 2 green"
@@ -31,7 +33,6 @@ fn solution1(filename: &str, max_cubes: &HashMap<&str, u8>) {
         let shown_cubes: String = it.by_ref().collect();
 
         // Extract highest number of cubes from each releave and save in hashmap
-        let mut cubes: HashMap<&str, u8> = HashMap::new();
         for cubes_releaved in shown_cubes.trim().split("; ") {
             // "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
             let mut splitted = cubes_releaved.split(", ");
@@ -39,25 +40,44 @@ fn solution1(filename: &str, max_cubes: &HashMap<&str, u8>) {
                 // "3 blue, 4 red"
                 let mut splitted = single_cube.split(" ");
                 let count: u8 = splitted.next().unwrap().parse().unwrap();
-                let color = splitted.next().unwrap();
+                let color = splitted.next().unwrap().to_string();
                 cubes.entry(color)
                     .and_modify(|stored_count| *stored_count = max(*stored_count, count))
                     .or_insert(count);
             }
         }
 
-        // Check is this game is valid
-        let mut valid_game = true;
+        Game {
+            game,
+            cubes
+        }
+    }
 
+    pub fn is_valid(&self, max_cubes: &HashMap<String, u8>) -> bool {
         for (color, count) in max_cubes {
-            if let Some(stored_count) = cubes.get(color) {
-                valid_game &= stored_count <= count;
+            if let Some(stored_count) = self.cubes.get(color) {
+                if *stored_count > *count {
+                    return false
+                }
             }
         }
 
-        println!("{} is valid {}", game, valid_game);
-        if valid_game {
-            result += game as u32;
+        true
+    }
+}
+
+fn solution1(filename: &str,  max_cubes: &HashMap<String, u8>) {
+    println!("Calibrating file {}", filename);
+
+    let mut result = 0;
+
+    for line in fs::read_to_string(filename).expect("Could not read file").lines() {
+        let game = Game::from_line(line);
+
+        // Check is this game is valid
+        if game.is_valid(&max_cubes) {
+            println!("Is valid {}", game.game);
+            result += game.game;
         }
     }
 
