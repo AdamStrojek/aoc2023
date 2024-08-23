001b1f671c495b50a5dc09c4b4a71b09206d684f
Adam Strojek <adam@strojek.info>
2024-08-23 20:00:54 +0200

    Add brute force solution for Day 5 Part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,11 +1,16 @@
 use std::fs;
 
 fn main() {
-    solution1("example.txt");
-    solution1("input.txt");
+    solution("example.txt", seed_parser_1);
+    solution("input.txt", seed_parser_1);
+    solution("example.txt", seed_parser_2);
+    solution("input.txt", seed_parser_2);
 }
 
-fn solution1(filename: &str) {
+fn solution<F>(filename: &str, seeds_parser: F)
+where
+    F: Fn(&str) -> Vec<u64>,
+{
     println!("Solving for file {}", filename);
 
     let mut locations = Vec::new();
@@ -17,9 +22,8 @@ fn solution1(filename: &str) {
     // seeds: 79 14 55 13
     // 012345678901234567
     //        ^ safely can skip here
-    let seeds = it.next().unwrap().strip_prefix("seeds: ").unwrap()
-        .split_whitespace().map(|seed| seed.parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
+    let seeds = seeds_parser(it.next().unwrap());
+    println!("Seeds calculated {}", seeds.len());
 
     // Parse mappings
     let mut mappers = Vec::<Mapper>::new();
@@ -40,6 +44,26 @@ fn solution1(filename: &str) {
 
     locations.sort();
     println!("Solution 1: {}", locations.first().unwrap());
+}
+
+fn seed_parser_1(s: &str) -> Vec<u64> {
+    s.strip_prefix("seeds: ").unwrap()
+     .split_whitespace().map(|seed| seed.parse::<u64>().unwrap())
+     .collect::<Vec<u64>>()
+}
+
+fn seed_parser_2(s: &str) -> Vec<u64> {
+    let mut result = Vec::new();
+
+    for chunk in s.strip_prefix("seeds: ").unwrap()
+        .split_whitespace().map(|seed| seed.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>().chunks(2) {
+        for i in 0..chunk[1] {
+            result.push(chunk[0] + i);
+        }
+    }
+
+    result
 }
 
 #[derive(Debug, PartialEq)]
